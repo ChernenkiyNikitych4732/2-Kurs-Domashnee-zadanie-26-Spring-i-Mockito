66fa6861215828091ef5bd09c5185e82e963f48c
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2023-09-07 16:54:05 +0200

    2 Курс ДЗ №26 Spring и Mockito изначальный комментарий

---
+++ src/main/java/sky/pro/course2homework/springandmockito/Kurs2Domasheezadanie26SpringiMockitoApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.course2homework.springandmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs2Domasheezadanie26SpringiMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs2Domasheezadanie26SpringiMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/course2homework/springandmockito/Kurs2Domasheezadanie26SpringiMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.course2homework.springandmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs2Domasheezadanie26SpringiMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
